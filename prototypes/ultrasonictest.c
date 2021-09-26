9ba1daa751fd98b1095b073997e924fd83dd9a10
Ismail Mohammed <ismmoh@kth.se>
2021-09-27 00:02:14 +0200

    Update ultrasonictest.c

--- prototypes/ultrasonictest.c
+++ prototypes/ultrasonictest.c
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include "brick.h"
 #include <stdio.h>
 #define MOTOR_LEFT OUTB
@@ -24,11 +32,14 @@ int main() {
 	
 
 	if(sensor_is_plugged(SENSOR_ULTRASONIC, SENSOR_TYPE__NONE_)) {
-		
+		/* Sätter läget på ultrasonic sensorn till att mäta kontinuerligt i centimeter. */
 		us_set_mode_us_dist_cm(SENSOR_ULTRASONIC);
 
 		while(1) {
-			
+			/*
+			 * Ultrasonic sensorn lägger in längden från en vägg i value0.
+			 * Den mäter med en decimal så värdet måste delas med 10 för att få centimeter.
+			 */
 			float distance = sensor_get_value0(SENSOR_ULTRASONIC, 0) / 10;
 
 			printf("Distans till vägg: %f cm\n", distance);
@@ -37,8 +48,9 @@ int main() {
 		tacho_stop(MOTOR_BOTH);  
 		
 			}
-			sleep_ms(500);
-		}
+		
+		  }
+    	}
 	}
 	else {
 		printf("Ultrasonic sensor not plugged in.\n");
