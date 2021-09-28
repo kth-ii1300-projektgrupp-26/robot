f5b2b078819bee941e87f3db0b422df78b66e170
m7tar <saw123789@gmail.com>
2021-09-28 16:58:33 +0200

    Update movement.c

--- src/movement.c
+++ src/movement.c
@@ -1,11 +1,15 @@
 #include "movement.h"
+#include "ports.h"
 
 float wheel_rotation_to_meter(float rotation) {
 	/* TODO */
+	int R_to_M = tacho_get_count_per_m(MOTOR_LEFT, 0 );
+
 }
 
 float meter_to_wheel_rotation(float length) {
 	/* TODO */
+	tacho_get_count_per_rot(, 0)
 }
 
 float rotate_robot(float degrees) {
