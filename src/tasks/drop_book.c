f00f7d9f4c33c1edf2b58862a4314f7306757b16
m7tar <saw123789@gmail.com>
2021-09-29 20:21:09 +0200

    drop book

--- src/tasks/drop_book.c
+++ src/tasks/drop_book.c
@@ -1,4 +1,6 @@
 #include "tasks/drop_book.h"
+#include "ports.h"
+#include "brick.h"
 
 void task_drop_book() {
 	/* TODO */
@@ -9,5 +11,14 @@ void subtask_position_for_drop() {
 }
 
 void subtask_drop_with_motor() {
-	/* TODO */
+	int motor_medium_speed = tacho_get_max_speed(MOTOR_MEDIUM,0) * 0.5;
+	tacho_set_speed_sp(MOTOR_MEDIUM, motor_medium_speed *0.5);
+	int motor_medium_degree = tacho_get_position(MOTOR_MEDIUM,0);
+
+	while( motor_medium_degree < 45)
+	{
+		tacho_run_forever(MOTOR_MEDIUM);
+		motor_medium_degree = tacho_get_position(MOTOR_MEDIUM,0);
+	}
+	tacho_stop(MOTOR_MEDIUM);
 }
