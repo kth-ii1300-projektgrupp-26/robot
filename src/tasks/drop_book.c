12cda73ea3e3935776bdd9cd2305f06bf68b3b25
m7tar <saw123789@gmail.com>
2021-10-01 10:21:08 +0200

    Update drop_book.c

--- src/tasks/drop_book.c
+++ src/tasks/drop_book.c
@@ -12,9 +12,9 @@ void subtask_position_for_drop() {
 
 void subtask_drop_with_motor() {
 	int motor_medium_speed = tacho_get_max_speed(MOTOR_MEDIUM,0) * 0.5;
-	tacho_set_speed_sp(MOTOR_MEDIUM, motor_medium_speed *0.5);
 	int motor_medium_degree = tacho_get_position(MOTOR_MEDIUM,0);
-
+	tacho_set_speed_sp(MOTOR_MEDIUM, motor_medium_speed *0.5);
+	
 	while( motor_medium_degree < 45)
 	{
 		tacho_run_forever(MOTOR_MEDIUM);
