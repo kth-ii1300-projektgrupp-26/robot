793ae7876c2ebff33e2cd33d99b6ce75d0e344ae
Hannes Mann <hannesmann2000@gmail.com>
2021-10-05 15:41:20 +0200

    Lägg till fil för att bygga med Docker

--- build.sh
+++ build.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 # Kompilerar alla *.c filer till ett körbart program som ligger i bin/mailman.
-GCC_ARGS="-lev3dev-c -Isrc -Wall -Werror-implicit-function-declaration"
+
+# Docker kräver att den här GCC används
+GCC=arm-linux-gnueabi-gcc
+GCC_ARGS="-lev3dev-c -Isrc -I/usr/local/include -Wall -Werror-implicit-function-declaration"
 
 mkdir -p build bin
 
 # Sensors
 mkdir -p build/sensors
-gcc -c src/sensors/gyro.c $GCC_ARGS -o build/sensors/gyro.o
-gcc -c src/sensors/ultrasonic.c $GCC_ARGS -o build/sensors/ultrasonic.o
+$GCC -c src/sensors/gyro.c $GCC_ARGS -o build/sensors/gyro.o
+$GCC -c src/sensors/ultrasonic.c $GCC_ARGS -o build/sensors/ultrasonic.o
 
 # Tasks
 mkdir -p build/tasks
-gcc -c src/tasks/drop_book.c $GCC_ARGS -o build/tasks/drop_book.o
-gcc -c src/tasks/find_wall.c $GCC_ARGS -o build/tasks/find_wall.o
-gcc -c src/tasks/move_and_avoid.c $GCC_ARGS -o build/tasks/move_and_avoid.o
+$GCC -c src/tasks/drop_book.c $GCC_ARGS -o build/tasks/drop_book.o
+$GCC -c src/tasks/find_wall.c $GCC_ARGS -o build/tasks/find_wall.o
+$GCC -c src/tasks/move_and_avoid.c $GCC_ARGS -o build/tasks/move_and_avoid.o
 
 # General
-gcc -c src/movement.c $GCC_ARGS -o build/movement.o
-gcc -c src/main.c $GCC_ARGS -o build/main.o
+$GCC -c src/movement.c $GCC_ARGS -o build/movement.o
+$GCC -c src/main.c $GCC_ARGS -o build/main.o
 
-gcc build/main.o \
+$GCC build/main.o \
 	build/movement.o \
 	\
 	build/tasks/drop_book.o \
---
+++ docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Bygger i Docker och kopierar till roboten
+docker start ev3
+docker exec -t ev3 /home/compiler/robot/build.sh
+docker stop ev3
+
+echo ""
+echo "Kopierar till /home/robot/bin/mailman-$(date --iso-8601):"
+# https://stackoverflow.com/questions/4018503/is-there-a-date-time-format-that-does-not-have-spaces/4018531
+scp bin/mailman robot@ev3dev.local:/home/robot/bin/mailman-$(date --iso-8601)
